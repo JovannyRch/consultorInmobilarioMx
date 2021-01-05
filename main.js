1d9bb2b0e7ff0c802bcccec43c4413552670d656
Jovanny Rch <jovannyrch@gmail.com>
2021-01-05 14:55:10 -0600

    Updated animation hover on description

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
 AOS.init();
+
+
+
+$( "#item1" ).hover(
+  function() {
+    $("#description-1").addClass( "hide" );
+  }, function() {
+    $("#description-1").removeClass( "hide" );
+  }
+);
+
+
+$( "#item2" ).hover(
+  function() {
+    $("#description-2").addClass( "hide" );
+  }, function() {
+    $("#description-2").removeClass( "hide" );
+  }
+);
+
+
+$( "#item3" ).hover(
+  function() {
+    $("#description-3").addClass( "hide" );
+  }, function() {
+    $("#description-3").removeClass( "hide" );
+  }
+);
+
+
+
+$( "#item4" ).hover(
+  function() {
+    $("#description-4").addClass( "hide" );
+  }, function() {
+    $("#description-4").removeClass( "hide" );
+  }
+);
+
+
+$("#fb-icon").click(
+  function () {
+     //TODO: Link to FB page
+  }
+);
